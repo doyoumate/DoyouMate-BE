69a2683674b8f7afe47c5a3a037861a48cf88ea0
earlgrey02 <san06036@naver.com>
2024-02-17 18:18:28 +0900

    Feat: 누락된 Repository 어노테이션 추가

--- domain/src/main/kotlin/com/doyoumate/domain/lecture/repository/LectureRepository.kt
+++ domain/src/main/kotlin/com/doyoumate/domain/lecture/repository/LectureRepository.kt
@@ -2,5 +2,7 @@ package com.doyoumate.domain.lecture.repository
 
 import com.doyoumate.domain.lecture.model.Lecture
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface LectureRepository : ReactiveMongoRepository<Lecture, String>
