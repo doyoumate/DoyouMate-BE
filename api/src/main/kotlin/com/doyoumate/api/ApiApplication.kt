625055f448a678e2cab0edf418376a4494bc0638
earlgrey02 <san06036@naver.com>
2024-02-17 17:14:00 +0900

    Refactor: ComponentScan 관련 리팩토링

--- api/src/main/kotlin/com/doyoumate/api/ApiApplication.kt
+++ api/src/main/kotlin/com/doyoumate/api/ApiApplication.kt
@@ -2,8 +2,10 @@ package com.doyoumate.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
-@SpringBootApplication(scanBasePackages = ["com.doyoumate.domain", "com.doyoumate.common"])
+@ComponentScan("com.doyoumate.api", "com.doyoumate.domain", "com.doyoumate.common")
+@SpringBootApplication
 class ApiApplication
 
 fun main(args: Array<String>) {
--- batch/src/main/kotlin/com/doyoumate/batch/BatchApplication.kt
+++ batch/src/main/kotlin/com/doyoumate/batch/BatchApplication.kt
@@ -2,8 +2,10 @@ package com.doyoumate.batch
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
-@SpringBootApplication(scanBasePackages = ["com.doyoumate.domain", "com.doyoumate.common"])
+@ComponentScan("com.doyoumate.batch", "com.doyoumate.domain", "com.doyoumate.common")
+@SpringBootApplication
 class BatchApplication
 
 fun main(args: Array<String>) {
