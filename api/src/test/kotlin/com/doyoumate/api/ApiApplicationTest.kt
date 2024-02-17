44d5c006647375a35e8a5830c0cef755b5ec18e3
earlgrey02 <san06036@naver.com>
2024-02-17 17:13:19 +0900

    Refactor: Application 테스트 리팩토링

---
+++ api/src/test/kotlin/com/doyoumate/api/ApiApplicationTest.kt
@@ -0,0 +1,6 @@
+package com.doyoumate.api
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTest
---
+++ batch/src/test/kotlin/com/doyoumate/batch/BatchApplicationTest.kt
@@ -0,0 +1,6 @@
+package com.doyoumate.batch
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BatchApplicationTest
