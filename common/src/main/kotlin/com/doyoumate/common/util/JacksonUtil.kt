066b46a0fa57f980ebc882b24f1ff543edb2f485
earlgrey02 <san06036@naver.com>
2024-02-17 22:50:37 +0900

    Feat: XML에서 'ROW'를 가져오는 유틸 메서드 구현

--- common/src/main/kotlin/com/doyoumate/common/util/JacksonUtil.kt
+++ common/src/main/kotlin/com/doyoumate/common/util/JacksonUtil.kt
@@ -1,6 +1,11 @@
 package com.doyoumate.common.util
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toFlux
+import reactor.kotlin.core.publisher.toMono
 
 inline fun <reified T> JsonNode.getValue(key: String): T =
     get(key)
@@ -16,3 +21,14 @@ inline fun <reified T> JsonNode.getValue(key: String): T =
                 else -> it
             } as T
         }
+
+fun XmlMapper.getRows(xml: String): Flux<JsonNode> =
+    readTree(xml)
+        .findValue("data")
+        ?.run { if (isArray) toFlux() else Flux.just(this) } ?: Flux.empty()
+
+fun XmlMapper.getRow(xml: String): Mono<JsonNode> =
+    readTree(xml)
+        .findValue("data")
+        ?.run { if (isArray) first() else this }
+        .toMono()
