507621e1a0d52ce489e86c39a423f45eebe7f086
earlgrey02 <san06036@naver.com>
2024-02-17 21:48:22 +0900

    Feat: Jackson 유틸에서 getValue 메서드가 Long이나 Float 타입의 데이터도 반환할 수 있도록 수정

--- common/src/main/kotlin/com/doyoumate/common/util/JacksonUtil.kt
+++ common/src/main/kotlin/com/doyoumate/common/util/JacksonUtil.kt
@@ -10,6 +10,8 @@ inline fun <reified T> JsonNode.getValue(key: String): T =
         .let {
             when (T::class) {
                 Int::class -> it.toInt()
+                Long::class -> it.toLong()
+                Float::class -> it.toFloat()
                 Double::class -> it.toDouble()
                 else -> it
             } as T
