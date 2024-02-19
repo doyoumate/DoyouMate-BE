2e9912866ccea387e577e2b1422967e8b910791c
earlgrey02 <san06036@naver.com>
2024-02-19 16:34:30 +0900

    Chore: 문서화를 위한 Spring REST Docs 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
+    id("com.epages.restdocs-api-spec") version "0.17.1"
     id("java-library")
     id("java-test-fixtures")
     id("maven-publish")
@@ -27,6 +28,7 @@ subprojects {
         plugin("kotlin-spring")
         plugin("org.springframework.boot")
         plugin("io.spring.dependency-management")
+        plugin("com.epages.restdocs-api-spec")
         plugin("java-library")
         plugin("java-test-fixtures")
         plugin("maven-publish")
@@ -47,12 +49,15 @@ subprojects {
         implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
         implementation("io.github.microutils:kotlin-logging:3.0.5")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("org.springframework.restdocs:spring-restdocs-webtestclient")
         testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
         testImplementation("io.kotest:kotest-assertions-core:5.6.2")
         testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
         testImplementation("io.mockk:mockk:1.13.5")
         testImplementation("io.projectreactor:reactor-test")
         testImplementation("com.ninja-squad:springmockk:3.0.1")
+        testImplementation("com.epages:restdocs-api-spec-webtestclient:0.17.1")
+        testImplementation("com.epages:restdocs-api-spec:0.17.1")
     }
 
     tasks {
