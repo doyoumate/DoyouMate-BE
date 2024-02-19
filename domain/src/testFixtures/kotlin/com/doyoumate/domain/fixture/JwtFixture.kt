70d4ab89d81d2105260ff7784e498b76f93cfe40
earlgrey02 <san06036@naver.com>
2024-02-19 16:59:44 +0900

    Feat: JwtFixture 구현

---
+++ domain/src/testFixtures/kotlin/com/doyoumate/domain/fixture/JwtFixture.kt
@@ -0,0 +1,22 @@
+package com.doyoumate.domain.fixture
+
+import com.github.jwt.config.JwtConfiguration
+import com.github.jwt.security.JwtAuthentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+
+val SECRET_KEY = (1..100).map { ('a'..'z').random() }.joinToString("")
+const val ACCESS_TOKEN_EXPIRE = 5L
+const val REFRESH_TOKEN_EXPIRE = 10L
+val AUTHORITIES = setOf(SimpleGrantedAuthority("USER"))
+val jwtProvider = JwtConfiguration()
+    .jwtProvider(SECRET_KEY, ACCESS_TOKEN_EXPIRE, REFRESH_TOKEN_EXPIRE)
+
+fun createJwtAuthentication(
+    id: String = ID,
+    authorities: Set<GrantedAuthority> = AUTHORITIES
+): JwtAuthentication =
+    JwtAuthentication(
+        id = id,
+        authorities = authorities
+    )
