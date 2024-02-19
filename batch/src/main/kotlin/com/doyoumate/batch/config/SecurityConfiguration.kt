56e8392ad6496053ac0328034a10de64ae91b704
earlgrey02 <san06036@naver.com>
2024-02-19 16:59:10 +0900

    Feat: Spring Security 설정 클래스 SecurityConfiguration 클래스 구현

---
+++ batch/src/main/kotlin/com/doyoumate/batch/config/SecurityConfiguration.kt
@@ -0,0 +1,44 @@
+package com.doyoumate.batch.config
+
+import com.github.jwt.core.JwtProvider
+import com.github.jwt.security.ReactiveJwtFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository
+
+@EnableWebFluxSecurity
+@Configuration
+class SecurityConfiguration {
+    @Bean
+    fun filterChain(
+        http: ServerHttpSecurity,
+        jwtFilter: ReactiveJwtFilter
+    ): SecurityWebFilterChain =
+        with(http) {
+            csrf { it.disable() }
+            formLogin { it.disable() }
+            httpBasic { it.disable() }
+            logout { it.disable() }
+            requestCache { it.disable() }
+            securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+            exceptionHandling {
+                it.authenticationEntryPoint(HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED))
+            }
+            authorizeExchange {
+                it.anyExchange()
+                    .hasRole("ADMIN")
+            }
+            addFilterAt(jwtFilter, SecurityWebFiltersOrder.AUTHORIZATION)
+            build()
+        }
+
+    @Bean
+    fun jwtFilter(jwtProvider: JwtProvider): ReactiveJwtFilter =
+        ReactiveJwtFilter(jwtProvider)
+}
