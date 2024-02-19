49ec00332d148b463f0f9a0c9482d6bd62d1a885
earlgrey02 <san06036@naver.com>
2024-02-19 16:43:57 +0900

    Feat: 컨트롤러 테스트 환경을 구성해주는 ControllerTest 클래스 구현

---
+++ common/src/testFixtures/kotlin/com/doyoumate/common/controller/ControllerTest.kt
@@ -0,0 +1,27 @@
+package com.doyoumate.common.controller
+
+import io.kotest.core.spec.style.DescribeSpec
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.context.ApplicationContext
+import org.springframework.restdocs.RestDocumentationContextProvider
+import org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@WebFluxTest
+@AutoConfigureRestDocs
+abstract class ControllerTest : DescribeSpec() {
+    @Autowired
+    private lateinit var applicationContext: ApplicationContext
+
+    @Autowired
+    private lateinit var restDocumentation: RestDocumentationContextProvider
+
+    protected val webClient: WebTestClient by lazy {
+        WebTestClient.bindToApplicationContext(applicationContext)
+            .configureClient()
+            .filter(WebTestClientRestDocumentation.documentationConfiguration(restDocumentation))
+            .build()
+    }
+}
