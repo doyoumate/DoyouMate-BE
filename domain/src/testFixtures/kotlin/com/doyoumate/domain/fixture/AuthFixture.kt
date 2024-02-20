467771828b90f28abb0fcd959d3fee35f3a53653
earlgrey02 <san06036@naver.com>
2024-02-21 04:01:09 +0900

    Feat: 회원가입 관련 AuthFixture 추가

--- domain/src/testFixtures/kotlin/com/doyoumate/domain/fixture/AuthFixture.kt
+++ domain/src/testFixtures/kotlin/com/doyoumate/domain/fixture/AuthFixture.kt
@@ -1,6 +1,7 @@
 package com.doyoumate.domain.fixture
 
 import com.doyoumate.domain.auth.dto.request.SendCertificationRequest
+import com.doyoumate.domain.auth.dto.request.SignUpRequest
 import com.doyoumate.domain.auth.model.Certification
 
 const val CODE = "123456"
@@ -22,4 +23,13 @@ fun createSendCertificationRequest(
     SendCertificationRequest(
         studentId = studentId,
         to = to
+    )
+
+fun createSignUpRequest(
+    certification: Certification = createCertification(),
+    password: String = PASSWORD
+): SignUpRequest =
+    SignUpRequest(
+        certification = certification,
+        password = password
     )
