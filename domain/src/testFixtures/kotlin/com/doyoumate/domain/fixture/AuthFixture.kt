dda82e187170706c2ef4e43dbdeb8267b9d7773b
earlgrey02 <san06036@naver.com>
2024-02-20 22:46:38 +0900

    Feat: 테스트 환경에서 사용하는 StudentFixture 및 AuthFixture 구현

---
+++ domain/src/testFixtures/kotlin/com/doyoumate/domain/fixture/AuthFixture.kt
@@ -0,0 +1,25 @@
+package com.doyoumate.domain.fixture
+
+import com.doyoumate.domain.auth.dto.request.SendCertificationRequest
+import com.doyoumate.domain.auth.model.Certification
+
+const val CODE = "123456"
+const val FROM = "01012345678"
+
+fun createCertification(
+    studentId: String = ID,
+    code: String = CODE
+): Certification =
+    Certification(
+        studentId = studentId,
+        code = code
+    )
+
+fun createSendCertificationRequest(
+    studentId: String = ID,
+    to: String = PHONE_NUMBER
+): SendCertificationRequest =
+    SendCertificationRequest(
+        studentId = studentId,
+        to = to
+    )
