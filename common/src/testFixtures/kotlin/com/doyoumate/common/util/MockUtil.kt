6977a9b15ee44e54364b42bcbeb18e71754e63d9
earlgrey02 <san06036@naver.com>
2024-02-21 20:05:08 +0900

    Feat: Spring Security 관련 MockUtil 추가

--- common/src/testFixtures/kotlin/com/doyoumate/common/util/MockUtil.kt
+++ common/src/testFixtures/kotlin/com/doyoumate/common/util/MockUtil.kt
@@ -1,7 +1,10 @@
 package com.doyoumate.common.util
 
+import com.doyoumate.domain.fixture.createJwtAuthentication
 import io.mockk.MockKAdditionalAnswerScope
 import io.mockk.MockKStubScope
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
@@ -10,3 +13,12 @@ infix fun <T> MockKStubScope<Mono<T>, *>.returns(value: T?): MockKAdditionalAnsw
 
 infix fun <T> MockKStubScope<Flux<T>, *>.returns(values: Iterable<T>): MockKAdditionalAnswerScope<Flux<T>, *> =
     returns(Flux.fromIterable(values))
+
+fun withMockUser() {
+    SecurityContextHolder.getContext().authentication = createJwtAuthentication()
+}
+
+fun withMockAdmin() {
+    SecurityContextHolder.getContext().authentication =
+        createJwtAuthentication(authorities = setOf(SimpleGrantedAuthority("ADMIN")))
+}
