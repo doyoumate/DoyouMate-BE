b322ac076eea638df6d39a3893344a1674a6cca3
earlgrey02 <san06036@naver.com>
2024-02-22 17:03:30 +0900

    Feat: LectureHandler getFilter 메서드 구현

--- api/src/main/kotlin/com/doyoumate/api/lecture/handler/LectureHandler.kt
+++ api/src/main/kotlin/com/doyoumate/api/lecture/handler/LectureHandler.kt
@@ -37,4 +37,8 @@ class LectureHandler(
                     )
                 )
         }
+
+    fun getFilter(request: ServerRequest): Mono<ServerResponse> =
+        ServerResponse.ok()
+            .body(lectureService.getFilter())
 }
