b591d5de867f8bebd7eea77e2380e566accd9557
earlgrey02 <san06036@naver.com>
2024-02-22 12:12:13 +0900

    Feat: LectureService searchLectures 메서드 구현

--- api/src/main/kotlin/com/doyoumate/api/lecture/service/LectureService.kt
+++ api/src/main/kotlin/com/doyoumate/api/lecture/service/LectureService.kt
@@ -2,6 +2,9 @@ package com.doyoumate.api.lecture.service
 
 import com.doyoumate.domain.lecture.dto.response.LectureResponse
 import com.doyoumate.domain.lecture.exception.LectureNotFoundException
+import com.doyoumate.domain.lecture.model.enum.Section
+import com.doyoumate.domain.lecture.model.enum.Semester
+import com.doyoumate.domain.lecture.repository.CustomLectureRepository
 import com.doyoumate.domain.lecture.repository.LectureRepository
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
@@ -9,7 +12,8 @@ import reactor.core.publisher.Mono
 
 @Service
 class LectureService(
-    private val lectureRepository: LectureRepository
+    private val lectureRepository: LectureRepository,
+    private val customLectureRepository: CustomLectureRepository
 ) {
     fun getLectureById(id: String): Mono<LectureResponse> =
         lectureRepository.findById(id)
@@ -18,5 +22,17 @@ class LectureService(
 
     fun getLectures(): Flux<LectureResponse> =
         lectureRepository.findAll()
+            .map { LectureResponse(it) }
+
+    fun searchLectures(
+        year: Int?,
+        grade: Int?,
+        semester: Semester?,
+        major: String?,
+        name: String,
+        credit: Int?,
+        section: Section?
+    ): Flux<LectureResponse> =
+        customLectureRepository.searchLectures(year, grade, semester, major, name, credit, section)
             .map { LectureResponse(it) }
 }
