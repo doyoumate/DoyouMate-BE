ccca77ade433867217daf6703fce980fa2e15e70
earlgrey02 <san06036@naver.com>
2024-02-23 20:13:32 +0900

    Feat: StudentHandler getStudentById 메서드 구현

---
+++ api/src/main/kotlin/com/doyoumate/api/student/handler/StudentHandler.kt
@@ -0,0 +1,17 @@
+package com.doyoumate.api.student.handler
+
+import com.doyoumate.api.student.service.StudentService
+import com.doyoumate.common.annotation.Handler
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.body
+import reactor.core.publisher.Mono
+
+@Handler
+class StudentHandler(
+    private val studentService: StudentService
+) {
+    fun getStudentById(request: ServerRequest): Mono<ServerResponse> =
+        ServerResponse.ok()
+            .body(studentService.getStudentById(request.pathVariable("id")))
+}
