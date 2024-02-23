e27d89346dc0a334f3b7188241102fcef476db3b
earlgrey02 <san06036@naver.com>
2024-02-23 22:13:57 +0900

    Feat: 강의 평가 관련 DTO 추가

---
+++ domain/src/main/kotlin/com/doyoumate/domain/review/dto/request/UpdateReviewRequest.kt
@@ -0,0 +1,11 @@
+package com.doyoumate.domain.review.dto.request
+
+import com.doyoumate.domain.review.model.Review
+
+data class UpdateReviewRequest(
+    val score: Int,
+    val content: String,
+) {
+    fun updateEntity(review: Review): Review =
+        review.copy(score = score, content = content)
+}
