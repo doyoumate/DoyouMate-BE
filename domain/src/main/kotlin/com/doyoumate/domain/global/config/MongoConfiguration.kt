06cb6fbd9181b7730e2364735ce73805633fc2c9
earlgrey02 <san06036@naver.com>
2024-02-23 22:12:34 +0900

    Feat: MongoDB Auditing 설정

--- domain/src/main/kotlin/com/doyoumate/domain/global/config/MongoConfiguration.kt
+++ domain/src/main/kotlin/com/doyoumate/domain/global/config/MongoConfiguration.kt
@@ -1,8 +1,10 @@
 package com.doyoumate.domain.global.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
 
+@EnableReactiveMongoAuditing
 @EnableReactiveMongoRepositories("com.doyoumate.domain")
 @Configuration
 class MongoConfiguration
