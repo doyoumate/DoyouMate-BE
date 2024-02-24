af25e6aa676fbc0200fe0f7234a68c5f7446e44a
earlgrey02 <san06036@naver.com>
2024-02-24 20:18:28 +0900

    Feat: 페이지네이션 관련 CommonFixture 추가

--- domain/src/testFixtures/kotlin/com/doyoumate/domain/fixture/CommonFixture.kt
+++ domain/src/testFixtures/kotlin/com/doyoumate/domain/fixture/CommonFixture.kt
@@ -1,6 +1,16 @@
 package com.doyoumate.domain.fixture
 
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import java.time.LocalDate
 
 const val ID = "id"
 val CREATED_DATE = LocalDate.now()!!
+const val PAGE = 0
+const val SIZE = 1
+
+fun createPageable(
+    page: Int = PAGE,
+    size: Int = SIZE
+): Pageable =
+    PageRequest.of(page, size)
