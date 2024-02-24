9e36a3047ebc10f5802630cd58998b7a859aee30
earlgrey02 <san06036@naver.com>
2024-02-24 20:03:51 +0900

    Feat: ReviewHandler Review CRUD 구현

---
+++ api/src/main/kotlin/com/doyoumate/api/review/handler/ReviewHandler.kt
@@ -0,0 +1,54 @@
+package com.doyoumate.api.review.handler
+
+import com.doyoumate.api.global.config.getAuthentication
+import com.doyoumate.api.review.service.ReviewService
+import com.doyoumate.common.annotation.Handler
+import com.doyoumate.common.util.component1
+import com.doyoumate.common.util.component2
+import com.doyoumate.domain.review.dto.request.CreateReviewRequest
+import com.doyoumate.domain.review.dto.request.UpdateReviewRequest
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.body
+import org.springframework.web.reactive.function.server.bodyToMono
+import reactor.core.publisher.Mono
+
+@Handler
+class ReviewHandler(
+    private val reviewService: ReviewService
+) {
+    fun getReviewsByStudentId(request: ServerRequest): Mono<ServerResponse> =
+        ServerResponse.ok()
+            .body(reviewService.getReviewsByStudentId(request.pathVariable("studentId")))
+
+    fun getReviewsByLectureId(request: ServerRequest): Mono<ServerResponse> =
+        ServerResponse.ok()
+            .body(reviewService.getReviewsByLectureId(request.pathVariable("lectureId")))
+
+    fun createReview(request: ServerRequest): Mono<ServerResponse> =
+        with(request) {
+            Mono.zip(bodyToMono<CreateReviewRequest>(), getAuthentication())
+                .flatMap { (createReviewRequest, authentication) ->
+                    ServerResponse.ok()
+                        .body(reviewService.createReview(createReviewRequest, authentication))
+                }
+        }
+
+    fun updateReviewById(request: ServerRequest): Mono<ServerResponse> =
+        with(request) {
+            Mono.zip(bodyToMono<UpdateReviewRequest>(), getAuthentication())
+                .flatMap { (updateReviewRequest, authentication) ->
+                    ServerResponse.ok()
+                        .body(reviewService.updateReviewById(pathVariable("id"), updateReviewRequest, authentication))
+                }
+        }
+
+    fun deleteReviewById(request: ServerRequest): Mono<ServerResponse> =
+        with(request) {
+            getAuthentication()
+                .flatMap {
+                    ServerResponse.ok()
+                        .body(reviewService.deleteReviewById(pathVariable("id"), it))
+                }
+        }
+}
