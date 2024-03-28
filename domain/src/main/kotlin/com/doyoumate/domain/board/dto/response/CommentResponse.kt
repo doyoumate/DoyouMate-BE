54214ade0d3b83501488df5a4ffa58dabfe38bff
earlgrey02 <san06036@naver.com>
2024-03-28 16:24:13 +0900

    Feat: 댓글 CRUD 관련 DTO 추가

---
+++ domain/src/main/kotlin/com/doyoumate/domain/board/dto/response/CommentResponse.kt
@@ -0,0 +1,25 @@
+package com.doyoumate.domain.board.dto.response
+
+import com.doyoumate.domain.board.model.Comment
+import java.time.LocalDateTime
+
+data class CommentResponse(
+    val id: String,
+    val postId: String,
+    val writerId: String,
+    val content: String,
+    val createdDate: LocalDateTime
+) {
+    companion object {
+        operator fun invoke(comment: Comment): CommentResponse =
+            with(comment) {
+                CommentResponse(
+                    id = id!!,
+                    postId = postId,
+                    writerId = writerId,
+                    content = content,
+                    createdDate = createdDate
+                )
+            }
+    }
+}
