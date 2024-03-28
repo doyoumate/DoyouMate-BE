76fe503496f1d22729a5fac775432ad565e3c12b
earlgrey02 <san06036@naver.com>
2024-03-28 15:15:23 +0900

    Feat: Post 엔티티 클래스 구현

---
+++ domain/src/main/kotlin/com/doyoumate/domain/board/model/Post.kt
@@ -0,0 +1,19 @@
+package com.doyoumate.domain.board.model
+
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+
+@Document
+data class Post(
+    @Id
+    val id: String,
+    val boardId: String,
+    val writerId: String,
+    val title: String,
+    val content: String,
+    val likedUserIds: List<String>,
+    @CreatedDate
+    val createdDate: LocalDateTime
+)
