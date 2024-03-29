2b1d04073abb23f586c194b9b01d7220df6cead1
earlgrey02 <san06036@naver.com>
2024-03-30 03:43:32 +0900

    Feat: StudentService getAppliedStudentsByLectureId 및 getPreAppliedStudentsByLectureId 메서드 구현

--- api/src/main/kotlin/com/doyoumate/api/student/service/StudentService.kt
+++ api/src/main/kotlin/com/doyoumate/api/student/service/StudentService.kt
@@ -4,15 +4,24 @@ import com.doyoumate.domain.student.dto.response.StudentResponse
 import com.doyoumate.domain.student.exception.StudentNotFoundException
 import com.doyoumate.domain.student.repository.StudentRepository
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.switchIfEmpty
 
 @Service
 class StudentService(
-    private val studentRepository: StudentRepository
+    private val studentRepository: StudentRepository,
 ) {
     fun getStudentById(id: String): Mono<StudentResponse> =
         studentRepository.findById(id)
             .switchIfEmpty { Mono.error(StudentNotFoundException()) }
+            .map { StudentResponse(it) }
+
+    fun getAppliedStudentsByLectureId(lectureId: String): Flux<StudentResponse> =
+        studentRepository.findAllByAppliedLectureIdsContains(lectureId)
+            .map { StudentResponse(it) }
+
+    fun getPreAppliedStudentsByLectureId(lectureId: String): Flux<StudentResponse> =
+        studentRepository.findAllByPreAppliedLectureIdsContains(lectureId)
             .map { StudentResponse(it) }
 }
