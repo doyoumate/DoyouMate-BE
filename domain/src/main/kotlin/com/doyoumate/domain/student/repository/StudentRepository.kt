defac56588c541a62341db51867d1617b385e9c8
earlgrey02 <san06036@naver.com>
2024-03-30 03:43:09 +0900

    Feat: StudentRepository findAllByAppliedLectureIdsContains 및 findAllByPreAppliedLectureIdsContains 메서드 구현

--- domain/src/main/kotlin/com/doyoumate/domain/student/repository/StudentRepository.kt
+++ domain/src/main/kotlin/com/doyoumate/domain/student/repository/StudentRepository.kt
@@ -3,6 +3,11 @@ package com.doyoumate.domain.student.repository
 import com.doyoumate.domain.student.model.Student
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
 
 @Repository
-interface StudentRepository : ReactiveMongoRepository<Student, String>
+interface StudentRepository : ReactiveMongoRepository<Student, String> {
+    fun findAllByAppliedLectureIdsContains(lectureIds: String): Flux<Student>
+
+    fun findAllByPreAppliedLectureIdsContains(lectureIds: String): Flux<Student>
+}
