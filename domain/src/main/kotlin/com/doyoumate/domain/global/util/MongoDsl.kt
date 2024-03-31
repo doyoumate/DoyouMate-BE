e97850238942831e2fe167ddfea335bfc9502d18
earlgrey02 <san06036@naver.com>
2024-03-31 22:44:20 +0900

    Feat: MongoDsl Update 관련 DSL 구현

--- domain/src/main/kotlin/com/doyoumate/domain/global/util/MongoDsl.kt
+++ domain/src/main/kotlin/com/doyoumate/domain/global/util/MongoDsl.kt
@@ -3,15 +3,21 @@ package com.doyoumate.domain.global.util
 import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.core.query.Criteria.where
 import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Update
 import org.springframework.data.mongodb.core.query.isEqualTo
 
 fun query(init: QueryDsl.() -> Unit): Query =
     QueryDsl()
         .apply(init)
-        .build()
+        .query
+
+fun update(init: UpdateDsl.() -> Unit): Update =
+    UpdateDsl()
+        .apply(init)
+        .update
 
 class QueryDsl {
-    private val query = Query()
+    val query = Query()
 
     infix fun String.isEqualTo(value: Any?) {
         value?.let {
@@ -28,6 +34,20 @@ class QueryDsl {
     fun paging(pageable: Pageable) {
         query.with(pageable)
     }
+}
 
-    fun build(): Query = query
+class UpdateDsl {
+    val update = Update()
+
+    infix fun String.set(value: Any?) {
+        value?.let {
+            update.set(this, it)
+        }
+    }
+
+    infix fun String.setOnInsert(value: Any?) {
+        value?.let {
+            update.setOnInsert(this, it)
+        }
+    }
 }
