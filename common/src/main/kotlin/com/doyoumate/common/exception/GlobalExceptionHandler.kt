7c41e42355e060f260e8e3de3f82184fc148e01f
earlgrey02 <san06036@naver.com>
2024-04-02 20:56:12 +0900

    Feat: GlobalExceptionHandler에서 자세한 에러 메세지는 로그로 출력하지 않도록 변경

--- common/src/main/kotlin/com/doyoumate/common/exception/GlobalExceptionHandler.kt
+++ common/src/main/kotlin/com/doyoumate/common/exception/GlobalExceptionHandler.kt
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatusCode
-import org.springframework.http.MediaType
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
@@ -21,10 +20,9 @@ class GlobalExceptionHandler(
         with(exchange.response) {
             val errorResponse = ErrorResponse(exception)
 
-            logger.error { "${exception::class.simpleName}(\"${exception.message}\") at ${exception.stackTrace[0]}" }
+            logger.error { "${exception::class.simpleName} at ${exception.stackTrace[0]}" }
 
             statusCode = HttpStatusCode.valueOf(errorResponse.code)
-            headers.contentType = MediaType.APPLICATION_JSON
 
             writeWith(
                 bufferFactory()
