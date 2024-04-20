839e60b195ad17caccb3aa57cf69d5b040c77c5f
earlgrey02 <san06036@naver.com>
2024-04-20 20:53:59 +0900

    Feat: BatchApplication의 main 메서드에 프로세스 종료 로직 추가

--- batch/src/main/kotlin/com/doyoumate/batch/BatchApplication.kt
+++ batch/src/main/kotlin/com/doyoumate/batch/BatchApplication.kt
@@ -1,13 +1,15 @@
 package com.doyoumate.batch
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
+import kotlin.system.exitProcess
 
 @ComponentScan("com.doyoumate.batch", "com.doyoumate.domain", "com.doyoumate.common")
 @SpringBootApplication
 class BatchApplication
 
 fun main(args: Array<String>) {
-    runApplication<BatchApplication>(*args)
+    exitProcess(SpringApplication.exit(runApplication<BatchApplication>(*args)))
 }
