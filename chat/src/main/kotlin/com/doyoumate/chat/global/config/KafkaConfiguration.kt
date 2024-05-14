25c5a1f8fa14b0ad18d2de429ca4148415dbfd4b
earlgrey02 <san06036@naver.com>
2024-05-14 20:35:13 +0900

    Feat: Kafka 관련 설정 클래스 KafkaConfiguration 구현

---
+++ chat/src/main/kotlin/com/doyoumate/chat/global/config/KafkaConfiguration.kt
@@ -0,0 +1,56 @@
+package com.doyoumate.chat.global.config
+
+import com.doyoumate.domain.message.model.Message
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
+import org.springframework.kafka.support.serializer.JsonDeserializer
+import org.springframework.kafka.support.serializer.JsonSerializer
+import reactor.kafka.receiver.ReceiverOptions
+import reactor.kafka.sender.SenderOptions
+
+@Configuration
+class ProducerConfiguration(
+    private val properties: KafkaProperties
+) {
+    @Bean
+    fun reactiveKafkaProducerTemplate(): ReactiveKafkaProducerTemplate<String, Any> =
+        mapOf<String, Any>(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to properties.bootstrapServers.first(),
+        ).let {
+            ReactiveKafkaProducerTemplate(
+                SenderOptions.create<String, Any>(it)
+                    .withKeySerializer(StringSerializer())
+                    .withValueSerializer(JsonSerializer())
+            )
+        }
+}
+
+@Configuration
+class ConsumerConfiguration(
+    private val properties: KafkaProperties
+) {
+    @Bean("messageConsumer")
+    fun messageConsumer(): ReactiveKafkaConsumerTemplate<String, Message> =
+        ReactiveKafkaConsumerTemplate(createReceiverOptions("message-group", setOf("message")))
+
+    private inline fun <reified T> createReceiverOptions(
+        groupId: String,
+        topics: Set<String>,
+    ): ReceiverOptions<String, T> =
+        mapOf<String, Any>(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to properties.bootstrapServers.first(),
+            ConsumerConfig.GROUP_ID_CONFIG to groupId,
+        ).let {
+            ReceiverOptions.create<String, T>(it)
+                .subscription(topics)
+                .withKeyDeserializer(StringDeserializer())
+                .withValueDeserializer(JsonDeserializer<T>(T::class.java, false))
+        }
+}
