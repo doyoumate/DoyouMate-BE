3213125a1e484c5cb519f9ec281df3db62ba1c09
earlgrey02 <san06036@naver.com>
2024-05-14 20:36:50 +0900

    Feat: JWT 모듈 버전 업데이트

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-log4j2")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
-    implementation("com.github.earlgrey02:JWT:1.1.0")
+    implementation("com.github.earlgrey02:JWT:1.1.1")
     implementation("net.nurigo:sdk:4.3.0")
     testImplementation(testFixtures(project(":common")))
     testImplementation(testFixtures(project(":domain")))
