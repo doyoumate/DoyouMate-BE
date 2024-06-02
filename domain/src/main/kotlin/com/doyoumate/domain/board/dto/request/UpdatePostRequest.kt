0392baa76c206b6182ce819699b884921c4a837b
earlgrey02 <san06036@naver.com>
2024-06-03 01:43:37 +0900

    Feat: UpdatePostRequest가 이미지들을 가지도록 변경

--- domain/src/main/kotlin/com/doyoumate/domain/board/dto/request/UpdatePostRequest.kt
+++ domain/src/main/kotlin/com/doyoumate/domain/board/dto/request/UpdatePostRequest.kt
@@ -2,16 +2,34 @@ package com.doyoumate.domain.board.dto.request
 
 import com.doyoumate.domain.board.model.Board
 import com.doyoumate.domain.board.model.Post
+import org.springframework.http.codec.multipart.FilePart
+import org.springframework.http.codec.multipart.FormFieldPart
+import org.springframework.http.codec.multipart.Part
+import org.springframework.util.MultiValueMap
 
 data class UpdatePostRequest(
-    val board: Board,
+    val boardId: String,
     val title: String,
     val content: String,
+    val images: List<FilePart>,
 ) {
-    fun updateEntity(post: Post): Post =
+    companion object {
+        operator fun invoke(multipartData: MultiValueMap<String, Part>): UpdatePostRequest =
+            with(multipartData) {
+                UpdatePostRequest(
+                    boardId = (get("boardId")!!.first() as FormFieldPart).value(),
+                    title = (get("title")!!.first() as FormFieldPart).value(),
+                    content = (get("content")!!.first() as FormFieldPart).value(),
+                    images = (get("images") ?: emptyList()) as List<FilePart>
+                )
+            }
+    }
+
+    fun updateEntity(post: Post, board: Board, images: List<String>): Post =
         post.copy(
             board = board,
             title = title,
-            content = content
+            content = content,
+            images = images
         )
 }
