76a8c6704f1471df64b0254b32803b8067ff1002
earlgrey02 <san06036@naver.com>
2024-06-03 01:47:17 +0900

    Refactor: GlobalExceptionHandler 리팩토링

--- common/src/main/kotlin/com/doyoumate/common/exception/GlobalExceptionHandler.kt
+++ common/src/main/kotlin/com/doyoumate/common/exception/GlobalExceptionHandler.kt
@@ -3,24 +3,26 @@ package com.doyoumate.common.exception
 import com.doyoumate.common.dto.ErrorResponse
 import com.doyoumate.common.util.getLogger
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler
-import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
 import org.springframework.http.HttpStatusCode
+import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebExceptionHandler
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 
-@Configuration
+@Order(-2)
+@Component
 class GlobalExceptionHandler(
     private val objectMapper: ObjectMapper
-) : ErrorWebExceptionHandler {
+) : WebExceptionHandler {
     private val logger = getLogger()
 
     override fun handle(exchange: ServerWebExchange, exception: Throwable): Mono<Void> =
         with(exchange.response) {
             val errorResponse = ErrorResponse(exception)
 
-            logger.error { "${exception::class.simpleName} at ${exception.stackTrace[0]}" }
+            logger.error { "${exception::class.simpleName}(\"${exception.message}\") at ${exception.stackTrace[0]}" }
 
             statusCode = HttpStatusCode.valueOf(errorResponse.code)
 
