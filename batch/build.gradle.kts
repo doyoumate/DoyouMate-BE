283b7e4c9effd660f0d94191df3126138d4a79f4
earlgrey02 <san06036@naver.com>
2024-06-04 02:59:35 +0900

    Chore: 사용하지 않는 의존성 및 관련 클래스 삭제

--- batch/build.gradle.kts
+++ batch/build.gradle.kts
@@ -3,10 +3,8 @@ dependencies {
     implementation(project(":common"))
     implementation("org.springframework.boot:spring-boot-starter-log4j2")
     implementation("org.springframework.boot:spring-boot-starter-batch")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
     implementation("com.h2database:h2")
-    implementation("com.github.earlgrey02:JWT:1.1.0")
 }
 
 tasks {
