19e9302ae75cd1fdaf50c48f91a5872dfdaad466
earlgrey02 <san06036@naver.com>
2024-06-03 21:53:16 +0900

    Feat: collectList 메서드 관련 유틸 메서드 추가

--- common/src/main/kotlin/com/doyoumate/common/util/KotlinUtil.kt
+++ common/src/main/kotlin/com/doyoumate/common/util/KotlinUtil.kt
@@ -1,5 +1,7 @@
 package com.doyoumate.common.util
 
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import reactor.util.function.Tuple2
 import reactor.util.function.Tuple4
 
@@ -14,3 +16,5 @@ operator fun <T1, T2, T3, T4> Tuple4<T1, T2, T3, T4>.component2(): T2 = t2
 operator fun <T1, T2, T3, T4> Tuple4<T1, T2, T3, T4>.component3(): T3 = t3
 
 operator fun <T1, T2, T3, T4> Tuple4<T1, T2, T3, T4>.component4(): T4 = t4
+
+fun <T> Flux<T>.collectSet(): Mono<Set<T>> = collectList().map { it.toSet() }
