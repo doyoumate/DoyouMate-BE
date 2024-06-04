790e00eb8de8c7f7fa43017fee45f918c75a31e3
earlgrey02 <san06036@naver.com>
2024-06-05 00:04:55 +0900

    Feat: Professor 엔티티 클래스 구현

---
+++ domain/src/main/kotlin/com/doyoumate/domain/professor/model/Professor.kt
@@ -0,0 +1,13 @@
+package com.doyoumate.domain.professor.model
+
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
+data class Professor(
+    val id: String,
+    val name: String,
+    val phoneNumber: String,
+    val email: String,
+    val score: Float,
+    val role: String
+)
