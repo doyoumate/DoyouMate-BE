5934907579afc663fef185df5320ed2d55ce6fd6
earlgrey02 <san06036@naver.com>
2024-06-04 17:31:14 +0900

    Feat: LogUtil에 JSON 출력 유틸 관련 메서드 추가

--- common/src/main/kotlin/com/doyoumate/common/util/LogUtil.kt
+++ common/src/main/kotlin/com/doyoumate/common/util/LogUtil.kt
@@ -5,3 +5,7 @@ import mu.KotlinLogging
 import kotlin.reflect.jvm.jvmName
 
 inline fun <reified T> T.getLogger(): KLogger = KotlinLogging.logger(T::class.jvmName)
+
+fun String.toPrettyJson(): String =
+    replace(Regex("\\n"), "")
+        .replace(Regex("\\s+"), " ")
